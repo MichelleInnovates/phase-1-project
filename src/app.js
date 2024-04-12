6a23de2a3ddd24ae3110b3a583ea1182aef07167
Michelle Kinyua <azariahthesecond@gmail.com>
2024-04-12 13:38:34 +0300

    removed home link

--- src/app.js
+++ src/app.js
@@ -1,25 +1,23 @@
-const headers = new Headers({
-    "Content-Type": "application/json",
-    "x-api-key": "live_yDuKuQv4oaHAtyvyeQALwyOZ4BwqtrglXeJXqzvCkm0k9LF5lroqFSUPij4CokrL"
-  });
-  
-  var requestOptions = {
-    method: 'GET',
-    headers: headers,
-    redirect: 'follow'
-  };
-  
-  fetch("https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-    const breedSearch = document.querySelector('#breed-search');
-const breedInfo = document.querySelector('#breed-info');
+const searchBtn = document.querySelector('#search-btn');
+const resultsContainer = document.querySelector('#results-container');
 
-breedSearch.addEventListener('keyup', (event) => {
-  if (event.key === 'Enter') {
-    const breed = event.target.value;
-    fetch(`https://api.thedogapi.com/v1/breeds/search?q=${breed}`)
+searchBtn.addEventListener('click', () => {
+  const breedSearch = document.querySelector('#breed-search');
+  const breed = breedSearch.value;
+
+  if (breed) {
+    const headers = new Headers({
+      "Content-Type": "application/json",
+      "x-api-key": "live_yDuKuQv4oaHAtyvyeQALwyOZ4BwqtrglXeJXqzvCkm0k9LF5lroqFSUPij4CokrL"
+    });
+
+    var requestOptions = {
+      method: 'GET',
+      headers: headers,
+      redirect: 'follow'
+    };
+
+    fetch(`https://api.thedogapi.com/v1/breeds/search?q=${breed}`, requestOptions)
       .then(response => response.json())
       .then(data => {
         if (data.length > 0) {
@@ -28,14 +26,14 @@ breedSearch.addEventListener('keyup', (event) => {
             <p>${data[0].description}</p>
             <img src="${data[0].image.url}" alt="${data[0].name}">
           `;
-          breedInfo.innerHTML = breedInfoHtml;
+          resultsContainer.innerHTML = breedInfoHtml;
         } else {
-          breedInfo.innerHTML = '<p>No breed found.</p>';
+          resultsContainer.innerHTML = '<p>No breed found.</p>';
         }
       })
       .catch(error => {
         console.error(error);
-        breedInfo.innerHTML = '<p>An error occurred.</p>';
+        resultsContainer.innerHTML = '<p>An error occurred.</p>';
       });
   }
 });
